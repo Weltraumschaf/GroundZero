9d712508a2dd631b1a29a9307dc78f47bc0590a1
Weltraumschaf <ich@weltraumschaf.de>
2013-07-12 19:50:26 +0200

    Implement last two tests for suppression generator.

--- src/test/java/de/weltraumschaf/groundzero/SuppressionGeneratorTest.java
+++ src/test/java/de/weltraumschaf/groundzero/SuppressionGeneratorTest.java
@@ -100,7 +100,7 @@ public class SuppressionGeneratorTest {
         violation3.setMessage("blub");
         violation3.setSource("foobar");
         file.addViolation(violation3);
-        
+
         assertThat(sut.generate(report), is(equalTo(
                 "<?xml version=\"1.0\" encoding\"UTF-8\"?>\n"
                 + "<!DOCTYPE suppressions PUBLIC "
@@ -114,22 +114,88 @@ public class SuppressionGeneratorTest {
     }
 
     @Test
-    @Ignore
     public void generateThreeFileOneError() {
-        assertThat(sut.generate(new CheckstyleReport("foo")), is(equalTo(
+        final CheckstyleViolation violation = new CheckstyleViolation();
+        violation.setLine(23);
+        violation.setColumn(42);
+        violation.setMessage("foo");
+        violation.setSource("bar");
+
+        final CheckstyleFile file1 = new CheckstyleFile("Foo.java");
+        file1.addViolation(violation);
+        final CheckstyleFile file2 = new CheckstyleFile("Bar.java");
+        file2.addViolation(violation);
+        final CheckstyleFile file3 = new CheckstyleFile("Baz.java");
+        file3.addViolation(violation);
+
+        final CheckstyleReport report = new CheckstyleReport("foo");
+        report.addFile(file1);
+        report.addFile(file2);
+        report.addFile(file3);
+
+        assertThat(sut.generate(report), is(equalTo(
                 "<?xml version=\"1.0\" encoding\"UTF-8\"?>\n"
                 + "<!DOCTYPE suppressions PUBLIC "
                 + "\"-//Puppy Crawl//DTD Suppressions 1.1//EN\" "
-                + "\"http://www.puppycrawl.com/dtds/suppressions_1_1.dtd\">\n")));
+                + "\"http://www.puppycrawl.com/dtds/suppressions_1_1.dtd\">\n"
+                + "<suppressions>\n"
+                + "    <suppress files=\"Foo\\.java\" lines=\"23\" columns=\"42\" checks=\"bar\"/>\n"
+                + "    <suppress files=\"Bar\\.java\" lines=\"23\" columns=\"42\" checks=\"bar\"/>\n"
+                + "    <suppress files=\"Baz\\.java\" lines=\"23\" columns=\"42\" checks=\"bar\"/>\n"
+                + "</suppressions>\n")));
     }
 
     @Test
-    @Ignore
     public void generateThreeFileThreeError() {
-        assertThat(sut.generate(new CheckstyleReport("foo")), is(equalTo(
+        final CheckstyleViolation violation1 = new CheckstyleViolation();
+        violation1.setLine(23);
+        violation1.setColumn(42);
+        violation1.setMessage("foo");
+        violation1.setSource("bar");
+        final CheckstyleViolation violation2 = new CheckstyleViolation();
+        violation2.setLine(11);
+        violation2.setColumn(12);
+        violation2.setMessage("blaa");
+        violation2.setSource("foobar");
+        final CheckstyleViolation violation3 = new CheckstyleViolation();
+        violation3.setLine(14);
+        violation3.setColumn(15);
+        violation3.setMessage("foobar");
+        violation3.setSource("blub");
+
+        final CheckstyleFile file1 = new CheckstyleFile("Foo.java");
+        file1.addViolation(violation1);
+        file1.addViolation(violation2);
+        file1.addViolation(violation3);
+        final CheckstyleFile file2 = new CheckstyleFile("Bar.java");
+        file2.addViolation(violation1);
+        file2.addViolation(violation2);
+        file2.addViolation(violation3);
+        final CheckstyleFile file3 = new CheckstyleFile("Baz.java");
+        file3.addViolation(violation1);
+        file3.addViolation(violation2);
+        file3.addViolation(violation3);
+
+        final CheckstyleReport report = new CheckstyleReport("foo");
+        report.addFile(file1);
+        report.addFile(file2);
+        report.addFile(file3);
+
+        assertThat(sut.generate(report), is(equalTo(
                 "<?xml version=\"1.0\" encoding\"UTF-8\"?>\n"
                 + "<!DOCTYPE suppressions PUBLIC "
                 + "\"-//Puppy Crawl//DTD Suppressions 1.1//EN\" "
-                + "\"http://www.puppycrawl.com/dtds/suppressions_1_1.dtd\">\n")));
+                + "\"http://www.puppycrawl.com/dtds/suppressions_1_1.dtd\">\n"
+                + "<suppressions>\n"
+                + "    <suppress files=\"Foo\\.java\" lines=\"23\" columns=\"42\" checks=\"bar\"/>\n"
+                + "    <suppress files=\"Foo\\.java\" lines=\"11\" columns=\"12\" checks=\"foobar\"/>\n"
+                + "    <suppress files=\"Foo\\.java\" lines=\"14\" columns=\"15\" checks=\"blub\"/>\n"
+                + "    <suppress files=\"Bar\\.java\" lines=\"23\" columns=\"42\" checks=\"bar\"/>\n"
+                + "    <suppress files=\"Bar\\.java\" lines=\"11\" columns=\"12\" checks=\"foobar\"/>\n"
+                + "    <suppress files=\"Bar\\.java\" lines=\"14\" columns=\"15\" checks=\"blub\"/>\n"
+                + "    <suppress files=\"Baz\\.java\" lines=\"23\" columns=\"42\" checks=\"bar\"/>\n"
+                + "    <suppress files=\"Baz\\.java\" lines=\"11\" columns=\"12\" checks=\"foobar\"/>\n"
+                + "    <suppress files=\"Baz\\.java\" lines=\"14\" columns=\"15\" checks=\"blub\"/>\n"
+                + "</suppressions>\n")));
     }
 }
