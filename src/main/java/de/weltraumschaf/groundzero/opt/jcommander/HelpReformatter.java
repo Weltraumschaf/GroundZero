fd0b42baa1a4ee9780e8156e77136baae2757299
Weltraumschaf <ich@weltraumschaf.de>
2013-08-04 14:23:19 +0200

    Fix checkstyle.

--- src/main/java/de/weltraumschaf/groundzero/opt/jcommander/HelpReformatter.java
+++ src/main/java/de/weltraumschaf/groundzero/opt/jcommander/HelpReformatter.java
@@ -27,22 +27,50 @@ import org.apache.commons.lang3.Validate;
  */
 public class HelpReformatter {
 
-    private static final String NEWLINE_REGEX = "\\r?\\n";
+    /**
+     * Used as line break in output strings.
+     */
     static final char NL = '\n';
+    /**
+     * Regular expression to split string by line brakes.
+     */
+    private static final String NEWLINE_REGEX = "\\r?\\n";
+    /**
+     * Number of white spaces to precede each option line.
+     */
+    private static final int SWITCH_PAD = 4;
+    /**
+     * List of strings to reformat.
+     */
     private List<String> input = Collections.emptyList();
 
+    /**
+     * Forwards to {@link #setInput(java.lang.String)}.
+     *
+     * @param input must not be {@link null}
+     */
     public void setInput(final StringBuilder input) {
         Validate.notNull(input);
         setInput(input.toString());
 
     }
 
-    public void setInput(final String input) {
-        Validate.notNull(input);
-        this.input = Lists.newArrayList(input.split(NEWLINE_REGEX));
+    /**
+     * Splits input string by line brakes and sets {@link #input}.
+     *
+     * @param raw must not be {@link null}
+     */
+    public void setInput(final String raw) {
+        Validate.notNull(raw);
+        this.input = Lists.newArrayList(raw.split(NEWLINE_REGEX));
 
     }
 
+    /**
+     * Get the usage line of the raw input.
+     *
+     * @return may be empty, never {@code null}
+     */
     public String getUsage() {
         if (input.isEmpty()) {
             return "";
@@ -51,6 +79,11 @@ public class HelpReformatter {
         return input.get(0).trim();
     }
 
+    /**
+     * Get the reformatted options of the raw input.
+     *
+     * @return may be empty, never {@code null}
+     */
     public String getOptions() {
         if (input.size() < 2) {
             return "";
@@ -60,7 +93,7 @@ public class HelpReformatter {
         buffer.append(input.get(1).trim()).append(NL);
 
         final Entries entries = extractEntries(input.subList(2, input.size()));
-        final int switchPadLength = entries.maxSwitchesLength() + 4;
+        final int switchPadLength = entries.maxSwitchesLength() + SWITCH_PAD;
 
         for (final Entry e : entries.getEntries()) {
             buffer.append("    ").append(rightPad(e.getSwitches(), switchPadLength))
@@ -71,12 +104,19 @@ public class HelpReformatter {
         return buffer.toString();
     }
 
+    /**
+     * Extracts option entries from list of lines.
+     *
+     * @param lines must not be {@code null}
+     * @return never {@code null}
+     */
     private Entries extractEntries(final Collection<String> lines) {
+        Validate.notNull(lines);
         final Entries entries = new Entries();
         int i = 0;
         String switches = "";
         String description = "";
-        String defaultValue = "";
+        String defaultValue;
 
         for (final String line : lines) {
             if (0 == i) {
@@ -90,7 +130,6 @@ public class HelpReformatter {
                 entries.add(new Entry(switches, description, defaultValue));
                 switches = "";
                 description = "";
-                defaultValue = "";
                 i = 0;
             }
         }
@@ -98,6 +137,13 @@ public class HelpReformatter {
         return entries;
     }
 
+    /**
+     * Pads a given string on the right side with white spaces.
+     *
+     * @param in string to pad
+     * @param length length to pad up, must not be negative
+     * @return never {@code null} may be empty
+     */
     String rightPad(final String in, final int length) {
         if (length < 0) {
             throw new IllegalArgumentException("Length must not be less than 0!");
@@ -112,43 +158,102 @@ public class HelpReformatter {
         return buffer.toString();
     }
 
+    /**
+     * Helper entity which holds all values of one option.
+     */
     private static class Entry {
 
+        /**
+         * The switches string (e.g. "-d, --debug").
+         */
         private final String switches;
+        /**
+         * The options description.
+         */
         private final String description;
+        /**
+         * The options default value.
+         */
         private final String defaultValue;
 
+        /**
+         * Dedicated constructor.
+         *
+         * @param switches must not be {@code null}
+         * @param description must not be {@code null}
+         * @param defaultValue must not be {@code null}
+         */
         public Entry(final String switches, final String description, final String defaultValue) {
+            Validate.notNull(switches, "Parameter switches must not be null!");
+            Validate.notNull(description, "Parameter description must not be null!");
+            Validate.notNull(defaultValue, "Parameter defaultValue must not be null!");
             this.switches = switches;
             this.description = description;
             this.defaultValue = defaultValue;
         }
 
+        /**
+         * Get switches string.
+         *
+         * @return never {@code nul}, may be empty
+         */
         public String getSwitches() {
             return switches;
         }
 
+        /**
+         * Get description string.
+         *
+         * @return never {@code nul}, may be empty
+         */
         public String getDescription() {
             return description;
         }
 
+        /**
+         * Get default value string.
+         *
+         * @return never {@code nul}, may be empty
+         */
         public String getDefaultValue() {
             return defaultValue;
         }
     }
 
+    /**
+     * Holds a whole collection of entries.
+     */
     private static class Entries {
 
+        /**
+         * The collected entries.
+         */
         private final Collection<Entry> entries = Lists.newArrayList();
 
-        public boolean add(final Entry e) {
-            return entries.add(e);
+        /**
+         * Add an entry.
+         *
+         * @param e must not be {@code null}
+         */
+        public void add(final Entry e) {
+            Validate.notNull(e);
+            entries.add(e);
         }
 
+        /**
+         * Get collected entries.
+         *
+         * @return never {@code null}, may be empty
+         */
         public Collection<Entry> getEntries() {
             return entries;
         }
 
+        /**
+         * Determines the length of the longest {@link Entry#getSwitches() switches} string of all collected entries.
+         *
+         * @return integer of range [0..n]
+         */
         public int maxSwitchesLength() {
             int length = 0;
 
@@ -160,5 +265,6 @@ public class HelpReformatter {
 
             return length;
         }
+
     }
 }
