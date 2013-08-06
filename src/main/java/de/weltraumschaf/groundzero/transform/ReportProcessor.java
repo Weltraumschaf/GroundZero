06027c3397d291491ef6fe71b18a66232072ec9b
Weltraumschaf <ich@weltraumschaf.de>
2013-08-06 19:58:51 +0200

    Remove unused import.

--- src/main/java/de/weltraumschaf/groundzero/transform/ReportProcessor.java
+++ src/main/java/de/weltraumschaf/groundzero/transform/ReportProcessor.java
@@ -23,7 +23,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
 import org.apache.commons.lang3.Validate;
-import de.weltraumschaf.commons.ApplicationException;
 import de.weltraumschaf.commons.IO;
 import java.io.UnsupportedEncodingException;
 import org.xml.sax.InputSource;
